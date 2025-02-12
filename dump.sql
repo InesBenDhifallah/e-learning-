9051e72b50b915c006af049dba868ad9942abef5
oumaimaghediri <oumaima.ghediri@esprit.tn>
2025-02-12 22:36:47 +0100

    Ajout du dump SQL de la base de données

--- dump.sql
+++ dump.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.39, for Win64 (x86_64)
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
 --
 -- Host: localhost    Database: pidev
 -- ------------------------------------------------------
--- Server version	5.5.5-10.4.32-MariaDB
+-- Server version	10.4.32-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,14 +21,15 @@
 
 DROP TABLE IF EXISTS `abonnement`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `abonnement` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(255) NOT NULL,
   `prix` double NOT NULL,
-  `durée` varchar(255) NOT NULL,
+  `date_debut` date DEFAULT NULL,
+  `date_fin` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,8 +38,67 @@ CREATE TABLE `abonnement` (
 
 LOCK TABLES `abonnement` WRITE;
 /*!40000 ALTER TABLE `abonnement` DISABLE KEYS */;
-INSERT INTO `abonnement` VALUES (1,'essai',100,'1 mois'),(2,'intermédiaire',250,'3 mois'),(3,'fidèle',700,'1 an');
 /*!40000 ALTER TABLE `abonnement` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chapitre`
+--
+
+DROP TABLE IF EXISTS `chapitre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chapitre` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cours_id` int(11) DEFAULT NULL,
+  `nomchapitre` varchar(255) NOT NULL,
+  `chapitres_id` int(11) DEFAULT NULL,
+  `courses_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_8C62B0257ECF78B0` (`cours_id`),
+  KEY `IDX_8C62B02520B9AB7E` (`chapitres_id`),
+  KEY `IDX_8C62B025F9295384` (`courses_id`),
+  CONSTRAINT `FK_8C62B02520B9AB7E` FOREIGN KEY (`chapitres_id`) REFERENCES `cours` (`id`),
+  CONSTRAINT `FK_8C62B0257ECF78B0` FOREIGN KEY (`cours_id`) REFERENCES `cours` (`id`),
+  CONSTRAINT `FK_8C62B025F9295384` FOREIGN KEY (`courses_id`) REFERENCES `cours` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chapitre`
+--
+
+LOCK TABLES `chapitre` WRITE;
+/*!40000 ALTER TABLE `chapitre` DISABLE KEYS */;
+/*!40000 ALTER TABLE `chapitre` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cours`
+--
+
+DROP TABLE IF EXISTS `cours`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cours` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nomcours` varchar(255) NOT NULL,
+  `matierecours` varchar(255) NOT NULL,
+  `niveaucours` varchar(255) NOT NULL,
+  `module_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_FDCA8C9CAFC2B591` (`module_id`),
+  CONSTRAINT `FK_FDCA8C9CAFC2B591` FOREIGN KEY (`module_id`) REFERENCES `module` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cours`
+--
+
+LOCK TABLES `cours` WRITE;
+/*!40000 ALTER TABLE `cours` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cours` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -47,7 +107,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `doctrine_migration_versions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `doctrine_migration_versions` (
   `version` varchar(191) NOT NULL,
   `executed_at` datetime DEFAULT NULL,
@@ -62,7 +122,7 @@ CREATE TABLE `doctrine_migration_versions` (
 
 LOCK TABLES `doctrine_migration_versions` WRITE;
 /*!40000 ALTER TABLE `doctrine_migration_versions` DISABLE KEYS */;
-INSERT INTO `doctrine_migration_versions` VALUES ('DoctrineMigrations\\Version20250210211107','2025-02-10 22:11:38',42),('DoctrineMigrations\\Version20250210211640','2025-02-10 22:16:56',8),('DoctrineMigrations\\Version20250210211845','2025-02-10 22:19:01',37),('DoctrineMigrations\\Version20250211212318','2025-02-11 22:23:35',25),('DoctrineMigrations\\Version20250211213302','2025-02-11 22:34:03',5);
+INSERT INTO `doctrine_migration_versions` VALUES ('DoctrineMigrations\\Version20250212194259','2025-02-12 20:43:52',11),('DoctrineMigrations\\Version20250212194608','2025-02-12 20:46:15',17),('DoctrineMigrations\\Version20250212194750','2025-02-12 20:48:10',47),('DoctrineMigrations\\Version20250212194943','2025-02-12 20:49:51',51),('DoctrineMigrations\\Version20250212195536','2025-02-12 20:55:44',106);
 /*!40000 ALTER TABLE `doctrine_migration_versions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -72,7 +132,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `messenger_messages`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `messenger_messages` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `body` longtext NOT NULL,
@@ -98,12 +158,35 @@ LOCK TABLES `messenger_messages` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `module`
+--
+
+DROP TABLE IF EXISTS `module`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `module` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_module` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `module`
+--
+
+LOCK TABLES `module` WRITE;
+/*!40000 ALTER TABLE `module` DISABLE KEYS */;
+/*!40000 ALTER TABLE `module` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `paiement`
 --
 
 DROP TABLE IF EXISTS `paiement`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `paiement` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
@@ -114,11 +197,10 @@ CREATE TABLE `paiement` (
   `cvv` int(11) NOT NULL,
   `montant` double NOT NULL,
   `id_abonnement_id` int(11) DEFAULT NULL,
-  `date_paiement` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_B1DC7A1E4FFF9576` (`id_abonnement_id`),
   CONSTRAINT `FK_B1DC7A1E4FFF9576` FOREIGN KEY (`id_abonnement_id`) REFERENCES `abonnement` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -127,7 +209,6 @@ CREATE TABLE `paiement` (
 
 LOCK TABLES `paiement` WRITE;
 /*!40000 ALTER TABLE `paiement` DISABLE KEYS */;
-INSERT INTO `paiement` VALUES (2,'ines','bendhifallahines@gmail.com','Visa',666,'2025-02-27',99,250,NULL,'2025-02-11'),(3,'ines','bendhifallahines@gmail.com','Visa',9,'2025-02-13',99,250,NULL,'2025-02-11'),(4,'asma','bendhifallahines@gmail.com','Mastercard',55,'2025-02-21',99,700,NULL,'2025-02-11'),(5,'asma','bendhifallahines@gmail.com','Mastercard',55,'2025-02-21',99,700,NULL,'2025-02-11');
 /*!40000 ALTER TABLE `paiement` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -140,4 +221,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-02-12 13:29:23
+-- Dump completed on 2025-02-12 22:36:23
