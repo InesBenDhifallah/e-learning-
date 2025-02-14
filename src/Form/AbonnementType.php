e7c5d51a6b29e9333c7754ffd54303eff1fc1c94
ines <bendhifallahines@gmail.com>
2025-02-14 16:12:02 +0100

    paiement et abonnement

---
+++ src/Form/AbonnementType.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Form;
+
+use App\Entity\Abonnement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class AbonnementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('type', TextType::class, [
+                'label' => 'Nom du Pack'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description'
+            ])
+            ->add('prix', NumberType::class, [
+                'label' => 'Prix'
+            ])
+            ->add('duree', TextType::class, [
+                'label' => 'Durée'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Abonnement::class,
+        ]);
+    }
+}
+
