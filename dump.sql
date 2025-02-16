5a48a67a1401b16fb3cf1e7b0175cc6c79444a15
oumaimaghediri <oumaima.ghediri@esprit.tn>
2025-02-16 19:55:29 +0100

    heyyyy

--- dump.sql
+++ dump.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
 --
 -- Host: localhost    Database: pidev
 -- ------------------------------------------------------
--- Server version	8.0.41
+-- Server version	10.4.32-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,15 +21,15 @@
 
 DROP TABLE IF EXISTS `abonnement`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `abonnement` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) NOT NULL,
   `prix` double NOT NULL,
-  `date_debut` date DEFAULT NULL,
-  `date_fin` date DEFAULT NULL,
+  `duree` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,6 +38,7 @@ CREATE TABLE `abonnement` (
 
 LOCK TABLES `abonnement` WRITE;
 /*!40000 ALTER TABLE `abonnement` DISABLE KEYS */;
+INSERT INTO `abonnement` VALUES (1,'essai',100,'1 mois',''),(2,'intermédiaire',250,'3 mois',''),(3,'fidèle',700,'1 an','');
 /*!40000 ALTER TABLE `abonnement` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -47,21 +48,15 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `chapitre`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `chapitre` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `cours_id` int DEFAULT NULL,
-  `nomchapitre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `chapitres_id` int DEFAULT NULL,
-  `courses_id` int DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `module_id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `IDX_8C62B0257ECF78B0` (`cours_id`),
-  KEY `IDX_8C62B02520B9AB7E` (`chapitres_id`),
-  KEY `IDX_8C62B025F9295384` (`courses_id`),
-  CONSTRAINT `FK_8C62B02520B9AB7E` FOREIGN KEY (`chapitres_id`) REFERENCES `cours` (`id`),
-  CONSTRAINT `FK_8C62B0257ECF78B0` FOREIGN KEY (`cours_id`) REFERENCES `cours` (`id`),
-  CONSTRAINT `FK_8C62B025F9295384` FOREIGN KEY (`courses_id`) REFERENCES `cours` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `IDX_8C62B025AFC2B591` (`module_id`),
+  CONSTRAINT `FK_8C62B025AFC2B591` FOREIGN KEY (`module_id`) REFERENCES `module` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,6 +65,7 @@ CREATE TABLE `chapitre` (
 
 LOCK TABLES `chapitre` WRITE;
 /*!40000 ALTER TABLE `chapitre` DISABLE KEYS */;
+INSERT INTO `chapitre` VALUES (1,3,'oumaima');
 /*!40000 ALTER TABLE `chapitre` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,17 +75,17 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `cours`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cours` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nomcours` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `matierecours` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `niveaucours` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `module_id` int DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) NOT NULL,
+  `chapitre_id` int(11) NOT NULL,
+  `contenu_fichier` varchar(255) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `IDX_FDCA8C9CAFC2B591` (`module_id`),
-  CONSTRAINT `FK_FDCA8C9CAFC2B591` FOREIGN KEY (`module_id`) REFERENCES `module` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `IDX_FDCA8C9C1FBEEF7B` (`chapitre_id`),
+  CONSTRAINT `FK_FDCA8C9C1FBEEF7B` FOREIGN KEY (`chapitre_id`) REFERENCES `chapitre` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,6 +94,7 @@ CREATE TABLE `cours` (
 
 LOCK TABLES `cours` WRITE;
 /*!40000 ALTER TABLE `cours` DISABLE KEYS */;
+INSERT INTO `cours` VALUES (2,'hhh',1,'76d19be5-2315-491a-bd25-7c8312a4c084-67b21e69c752f014830018.jpg','2025-02-16 18:20:41'),(3,'lll',1,'workshopb-fascicule3-67b21b4f0bd42337002557.docx','2025-02-16 18:07:27'),(5,'oo',1,'correction-examenfinaljava2015final-67b22efeb5f58898873894.pdf','2025-02-16 19:31:26'),(6,'jjjjjj',1,'deploiment-67b22f4a4c281403892770.png','2025-02-16 19:32:42');
 /*!40000 ALTER TABLE `cours` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -107,13 +104,13 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `doctrine_migration_versions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `doctrine_migration_versions` (
-  `version` varchar(191) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `version` varchar(191) NOT NULL,
   `executed_at` datetime DEFAULT NULL,
-  `execution_time` int DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL,
   PRIMARY KEY (`version`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -122,7 +119,7 @@ CREATE TABLE `doctrine_migration_versions` (
 
 LOCK TABLES `doctrine_migration_versions` WRITE;
 /*!40000 ALTER TABLE `doctrine_migration_versions` DISABLE KEYS */;
-INSERT INTO `doctrine_migration_versions` VALUES ('DoctrineMigrations\\Version20250212194259','2025-02-12 20:43:52',11),('DoctrineMigrations\\Version20250212194608','2025-02-12 20:46:15',17),('DoctrineMigrations\\Version20250212194750','2025-02-12 20:48:10',47),('DoctrineMigrations\\Version20250212194943','2025-02-12 20:49:51',51),('DoctrineMigrations\\Version20250212195536','2025-02-12 20:55:44',106);
+INSERT INTO `doctrine_migration_versions` VALUES ('DoctrineMigrations\\Version20250210211107','2025-02-10 22:11:38',42),('DoctrineMigrations\\Version20250210211640','2025-02-10 22:16:56',8),('DoctrineMigrations\\Version20250210211845','2025-02-10 22:19:01',37),('DoctrineMigrations\\Version20250211212318','2025-02-11 22:23:35',25),('DoctrineMigrations\\Version20250211213302','2025-02-11 22:34:03',5);
 /*!40000 ALTER TABLE `doctrine_migration_versions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -132,19 +129,19 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `enseignant`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `enseignant` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `phonenumber` bigint NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `matiere` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `experience` int NOT NULL,
-  `cv` longblob,
-  `reason` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `cvpath` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `phonenumber` bigint(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `matiere` varchar(255) NOT NULL,
+  `experience` int(11) NOT NULL,
+  `cv` longblob DEFAULT NULL,
+  `reason` varchar(255) NOT NULL,
+  `cvpath` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -153,8 +150,38 @@ CREATE TABLE `enseignant` (
 
 LOCK TABLES `enseignant` WRITE;
 /*!40000 ALTER TABLE `enseignant` DISABLE KEYS */;
-INSERT INTO `enseignant` VALUES (1,'aaa',123,'aaaa@azrazr.com','aaa',12,NULL,'zaerzar',NULL),(2,'aaa',123,'aaa@aaa.com','aaa',12,NULL,'aaaa',NULL);
 /*!40000 ALTER TABLE `enseignant` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `event`
+--
+
+DROP TABLE IF EXISTS `event`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `event` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `time` varchar(255) NOT NULL,
+  `place` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `speaker` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `created_by` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `event`
+--
+
+LOCK TABLES `event` WRITE;
+/*!40000 ALTER TABLE `event` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -163,12 +190,12 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `messenger_messages`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `messenger_messages` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `body` longtext NOT NULL,
+  `headers` longtext NOT NULL,
+  `queue_name` varchar(190) NOT NULL,
   `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
   `available_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
   `delivered_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)',
@@ -194,12 +221,12 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `module`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `module` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nom_module` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -208,6 +235,7 @@ CREATE TABLE `module` (
 
 LOCK TABLES `module` WRITE;
 /*!40000 ALTER TABLE `module` DISABLE KEYS */;
+INSERT INTO `module` VALUES (1,'ines'),(2,'ines'),(3,'oum');
 /*!40000 ALTER TABLE `module` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -217,21 +245,22 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `paiement`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `paiement` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `type_carte` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `num_carte` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `type_carte` varchar(255) NOT NULL,
+  `num_carte` int(11) NOT NULL,
   `date_expiration` date NOT NULL,
-  `cvv` int NOT NULL,
+  `cvv` int(11) NOT NULL,
   `montant` double NOT NULL,
-  `id_abonnement_id` int DEFAULT NULL,
+  `id_abonnement_id` int(11) DEFAULT NULL,
+  `date_paiement` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_B1DC7A1E4FFF9576` (`id_abonnement_id`),
   CONSTRAINT `FK_B1DC7A1E4FFF9576` FOREIGN KEY (`id_abonnement_id`) REFERENCES `abonnement` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -240,7 +269,37 @@ CREATE TABLE `paiement` (
 
 LOCK TABLES `paiement` WRITE;
 /*!40000 ALTER TABLE `paiement` DISABLE KEYS */;
+INSERT INTO `paiement` VALUES (2,'ines','bendhifallahines@gmail.com','Visa',666,'2025-02-27',99,250,NULL,'2025-02-11'),(3,'ines','bendhifallahines@gmail.com','Visa',9,'2025-02-13',99,250,NULL,'2025-02-11'),(4,'asma','bendhifallahines@gmail.com','Mastercard',55,'2025-02-21',99,700,NULL,'2025-02-11'),(5,'asma','bendhifallahines@gmail.com','Mastercard',55,'2025-02-21',99,700,NULL,'2025-02-11');
 /*!40000 ALTER TABLE `paiement` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `participation`
+--
+
+DROP TABLE IF EXISTS `participation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `participation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `event_id` int(11) NOT NULL,
+  `nbr_tickets` int(11) NOT NULL,
+  `somme` decimal(10,2) NOT NULL,
+  `payment_method` varchar(20) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_AB55E24F71F7E88B` (`event_id`),
+  CONSTRAINT `FK_AB55E24F71F7E88B` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `participation`
+--
+
+LOCK TABLES `participation` WRITE;
+/*!40000 ALTER TABLE `participation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `participation` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -249,21 +308,21 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `roles` longtext COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '(DC2Type:json)',
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `phonenumber` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `matiere` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `experience` int DEFAULT NULL,
-  `reason` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(180) NOT NULL,
+  `roles` longtext NOT NULL COMMENT '(DC2Type:json)',
+  `password` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `phonenumber` varchar(255) DEFAULT NULL,
+  `matiere` varchar(255) DEFAULT NULL,
+  `experience` int(11) DEFAULT NULL,
+  `reason` varchar(255) DEFAULT NULL,
   `is_verified` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_IDENTIFIER_EMAIL` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -272,7 +331,6 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'aaaa@azrazr.com','[\"ROLE_TEACHER\"]','$2y$13$Rd/wuGR1HVo8m9kJ57vYEOPK9UGLIAPoOURtzfjQ2Vom7lkOzaNsK','aaa','123456','mathematiques',12,'aerzer',0),(2,'zaeraezr@gmail.com','[\"ROLE_TEACHER\"]','$2y$13$wumCJA73t/eg5xMXAMDI0uVgb6Z74fDWh.SEi4gqhA7SOh6QVHynC','aaaa','123123','sciences',12,'azrazr',0),(3,'zaeraezaezeazer@gmail.com','[\"ROLE_TEACHER\"]','$2y$13$r3jFTOhIq/3wGVebP4NovucCLmb8.WhYdPmAFd.MxssveYCqAk2zy','aaaa','123123','sciences',12,'azrazr',0),(4,'aaaab@azrazr.com','[\"ROLE_TEACHER\"]','$2y$13$GqxhpC8pCXA6um4xkwsKvu3tuZJxIs.wTLenGM2awa5oUWozyZ2xK','azerezar','123123','sciences',14,'zzz',0),(5,'aaaaaaaaaaa@gmail.com','[\"ROLE_TEACHER\"]','$2y$13$VoPE6BfQqk26SwDBtcRBPuWDsWm0HG252sDdzJyB5CccBKc.Th/62','aaa','1234456','sciences',12,'aaa',0),(6,'nanous.bellagha@gmail.com','[\"ROLE_TEACHER\"]','$2y$13$v.zkuZkvKBk8Buy6eVbcFuLCNmjk7Mu5eGVj87ueOVOrR9TzakwmC','nanousti','123456','sciences',12,'aaa',0);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -285,4 +343,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-02-15 18:24:09
+-- Dump completed on 2025-02-16 19:46:27
