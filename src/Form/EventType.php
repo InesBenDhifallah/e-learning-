3d1888b6c2d5b50452d3e9854c0f8af20f82aa69
khlifi-ahmed <Khlifi.Ahmed@esprit.tn>
2025-02-17 12:48:22 +0100

    Initialized new gestion-event branch with latest changes

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -1,44 +1,68 @@
 <?php
+
 namespace App\Form;
 
 use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\TimeType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use App\Entity\User;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class EventType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('date', DateType::class, [
-                'widget' => 'single_text',
+            ->add('titre', TextType::class, [
+                'label' => 'Titre',
+                'attr' => ['placeholder' => 'Nom de l\'événement']
             ])
-            ->add('time', TextType::class, [
-                'label' => 'Time (HH:MM)',
-                'attr' => ['placeholder' => 'e.g., 14:30']
+            ->add('description', TextType::class, [
+                'label' => 'Description',
+                'attr' => ['placeholder' => 'Description de l\'événement']
             ])
-            ->add('place')
-            ->add('category')
-            ->add('speaker')
+            ->add('type', ChoiceType::class, [
+                'label' => 'Type d\'événement',
+                'choices' => [
+                    'En ligne' => 'enligne',
+                    'Présentiel' => 'presentiel'
+                ],
+                'expanded' => false,
+                'multiple' => false,
+                'attr' => [
+                    'id' => 'event-type', // Explicitly setting the ID
+                    'data-event-type' => 'true' // Extra attribute for JavaScript
+                ]
+            ])
+            ->add('localisation', TextType::class, [
+                'label' => 'Localisation',
+                'required' => false, // Should NOT be required when type is "enligne"
+                'attr' => [
+                    'placeholder' => 'Lieu de l\'événement',
+                    'data-localisation' => 'true' // Help JavaScript find it
+                ]
+            ])
+            ->add('dateDebut', DateType::class, [
+                'label' => 'Date de début',
+                'widget' => 'single_text'
+            ])
+            ->add('dateFin', DateType::class, [
+                'label' => 'Date de fin',
+                'widget' => 'single_text'
+            ])
+            ->add('prix', NumberType::class, [
+                'label' => 'Prix',
+                'attr' => ['placeholder' => 'Prix en €']
+            ])
             ->add('image', FileType::class, [
-                'required' => false, // Optional
-                'mapped' => false,   // If you're not saving the file directly to the entity
-            ])
-            //->add('created_by', EntityType::class, [
-              //  'class' => User::class,  // Make sure to set the appropriate entity
-              //  'choice_label' => 'username',  // Assuming you have a 'username' field in the User entity
-           // ])
-           ->add('created_by')
-        ;
+                'label' => 'Image',
+                'mapped' => false,
+                'required' => false
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
